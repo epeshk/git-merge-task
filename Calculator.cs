131701de40ae2b5b19e3038ccdb83c9000469949
Pavel Egorov <xoposhiy@mail.ru>
2015-04-23 20:41:00 +0500

    Calculator does not fail any more on error user input.

--- Calculator.cs
+++ Calculator.cs
@@ -9,6 +9,7 @@ namespace Kontur.Courses.Git
 			if (args.Length == 0)
 				return lastResult;
 			if (args.Length == 1)
+<<<<<<< f7e896915c6a9ad78e99edd2f8f76aecb24a924b
 				return lastResult = double.Parse(args[0]);
 			if (args.Length == 2)
 			{
@@ -20,13 +21,25 @@ namespace Kontur.Courses.Git
                 //>5
                 return lastResult = Execute(args[0], lastResult.Value, double.Parse(args[1]));
             }
+=======
+				return lastResult = TryParseDouble(args[0]);
+>>>>>>> Calculator does not fail any more on error user input.
 			if (args.Length == 3)
 			{
-				var v1 = double.Parse(args[0]);
-				var v2 = double.Parse(args[2]);
-				return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				return lastResult = Execute(args[1], v1.Value, v2.Value);
 			}
 			return Maybe<double>.FromError("Error input");
+		}
+
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
 		}
 
 		private Maybe<double> Execute(string op, double v1, double v2)
